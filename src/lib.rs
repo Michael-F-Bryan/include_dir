8e90d22070ba65f4ee9439a40b72410c3f88fffd
Michael-F-Bryan <michaelfbryan@gmail.com>
2017-06-07 23:10:36 +0800

    Updated the crate docs with copy/paste-able examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,32 @@
 //! The logical evolution of the `include_str!()` macro to allow embedding
 //! entire file trees.
+//!
+//! If you want to use this in stable, you'll need a `build.rs` build script.
+//!
+//! ```rust,no_run
+//! extern crate include_dir;
+//!
+//! use std::env;
+//! use std::path::Path;
+//! use include_dir::include_dir;
+//!
+//! fn main() {
+//!     let outdir = env::var("OUT_DIR").unwrap();
+//!     let dest_path = Path::new(&outdir).join("assets.rs");
+//!
+//!     include_dir("assets")
+//!         .as_variable("SRC")
+//!         .to_file(dest_path)
+//!         .unwrap();
+//!     }
+//! ```
+//!
+//! Then in one of your source files, you'll need to include the generated
+//! `assets.rs` file.
+//!
+//! ```rust,ignore
+//! include!(concat!(env!("OUT_DIR"), "/assets.rs"));
+//! ```
 
 #![deny(missing_docs,
         missing_debug_implementations, missing_copy_implementations,
