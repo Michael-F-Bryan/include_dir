69eff61eef506ed6c85de776cb34cd326cc108ac
Michael-F-Bryan <michaelfbryan@gmail.com>
2017-06-10 05:42:37 +0800

    Changed logging format and added pattern matching

--- integration_tests.py
+++ integration_tests.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 """
+Usage:
+    ./integration_tests.py
+    ./integration_tests.py <patterns>...
+
+Options:
+    -h, --help               Print this help information.
+
+
 Because a large part of this crate's functionality depends on generated code,
 it's easier to test functionality from the point-of-view of an end user.
 Therefore, a large proportion of the crate's tests are orchestrated by a
@@ -18,7 +26,6 @@ will:
 """
 
 import os
-import sys
 from pathlib import Path
 import subprocess
 import tempfile
@@ -26,6 +33,9 @@ import logging
 import shutil
 import re
 import time
+from concurrent.futures import ThreadPoolExecutor
+
+from docopt import docopt
 
 DEBUG = False
 
@@ -65,20 +75,23 @@ version = "0.1.0"
 include_dir = {{path = "{}"}}
 """
 
-def pretty_print_output(output):
-    logging.warn("return code: %d", output.returncode)
+def pretty_print_output(name, output):
+    logging.warning("return code: %d", output.returncode)
     if output.stdout:
         logging.warn("stdout:")
         for line in output.stdout.decode().split("\n"):
-            logging.warn("%s", line)
+            logging.warning("(%s) %s", name, line)
 
     if output.stderr:
-        logging.warn("stderr:")
+        logging.warning("stderr:")
         for line in output.stderr.decode().split("\n"):
-            logging.warn("%s", line)
+            logging.warning("(%s) %s", name, line)
 
 
 class IntegrationTest:
+    """
+    A runner for an integration test.
+    """
     def __init__(self, filename):
         self.script = filename.relative_to(project_root)
         self.name = self.script.stem
@@ -86,7 +99,11 @@ class IntegrationTest:
         self.crate = None
 
     def initialize(self):
-        logging.debug("Initializing test crate in %s", self.temp_dir.name)
+        """
+        Do all the work necessary to create a new project, copy across the test
+        file, add a build script, then adjust Cargo.toml accordingly.
+        """
+        logging.debug("(%s) Initializing test crate in %s", self.name, self.temp_dir.name)
         crate_name = self.name
 
         cmd = ["cargo", "new", "--bin", crate_name]
@@ -94,24 +111,27 @@ class IntegrationTest:
             cmd.append("--verbose")
 
         output = subprocess.run(cmd,
-                       cwd=self.temp_dir.name,
-                       stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE)
+                                cwd=self.temp_dir.name,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
 
         if output.returncode != 0:
             logging.error("Unable to create a new crate")
-            pretty_print_output(output)
+            pretty_print_output(self.name, output)
             return
 
         self.crate = Path(self.temp_dir.name) / crate_name
 
         shutil.copy(self.script, self.crate / "src" / "main.rs")
 
-        self.generate_build_rs()
-        self.update_cargo_toml()
-        self.copy_across_cache()
+        self._generate_build_rs()
+        self._update_cargo_toml()
+        self._copy_across_cache()
 
     def run(self):
+        """
+        Run the test, checking whether it passes or fails (non-zero exit code).
+        """
         logging.info('Running test "%s"', self.name)
 
         cmd = ["cargo", "run"]
@@ -120,35 +140,34 @@ class IntegrationTest:
             cmd.append("--verbose")
 
         output = subprocess.run(cmd,
-                       cwd=self.crate,
-                       stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE)
+                                cwd=self.crate,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
 
         if output.returncode != 0:
-            logging.error("Test failed ✘")
-            pretty_print_output(output)
-            return
+            logging.error('%-20s\t✘', self.name)
+            pretty_print_output(self.name, output)
         else:
-            logging.info("Pass ✔")
+            logging.info('%-20s\t✔', self.name)
 
 
-    def generate_build_rs(self):
-        asset_dir = self.assets_to_embed()
-        logging.debug("Generating build.rs (assets: %s)", asset_dir)
+    def _generate_build_rs(self):
+        asset_dir = self._assets_to_embed()
+        logging.debug("(%s) Generating build.rs (assets: %s)", self.name, asset_dir)
 
         build_rs = self.crate / "build.rs"
 
         with open(build_rs, "w") as f:
             f.write(BUILD_RS_TEMPLATE.format(asset_dir))
 
-    def update_cargo_toml(self):
-        logging.debug("Updating Cargo.toml")
+    def _update_cargo_toml(self):
+        logging.debug("(%s) Updating Cargo.toml", self.name)
         cargo_toml = self.crate / "Cargo.toml"
 
         with open(cargo_toml, "w") as f:
             f.write(CARGO_TOML_TEMPLATE.format(self.name, project_root))
 
-    def assets_to_embed(self):
+    def _assets_to_embed(self):
         # Search for the "special" pattern -> include_dir!("path/to/assets")
         pattern = re.compile(r'include_dir!\("([\w\d./]+)"\)')
 
@@ -157,32 +176,55 @@ class IntegrationTest:
             if got is None:
                 return project_root  / "src"
             else:
-                return Path(abspath(got.groups(1)))
+                return Path(os.path.abspath(got.groups(1)))
 
-    def copy_across_cache(self):
-        logging.debug('Copying across "target/" dir')
-        os.symlink(project_root / "target", self.crate / "target")
+    def _copy_across_cache(self):
+        logging.debug('(%s) Copying across "target/" dir', self.name)
+        try:
+            # os.symlink(project_root / "target", self.crate / "target")
+            shutil.copytree(project_root / "target", self.crate / "target")
+        except:
+            pass
 
     def __repr__(self):
         return '<{}: filename="{}">'.format(
             self.__class__.__name__,
-            self.filename)
+            self.name)
 
 
-def discover_integration_tests():
+def discover_integration_tests(patterns):
+    # Use a reasonable default if no patterns provided
+    if len(patterns) == 0:
+        patterns = ['*.rs']
+
     test_dir = project_root / "integration_tests"
+    filenames = set()
 
-    for file in test_dir.glob("*.rs"):
-        test = IntegrationTest(file)
-        yield test
+    for pattern in patterns:
+        matches = test_dir.glob(pattern)
+        for match in matches:
+            filenames.add(match)
+
+    return [IntegrationTest(name) for name in filenames]
 
 
-def main():
-    for test in discover_integration_tests():
-        test.initialize()
-        # time.sleep(12000)
-        test.run()
+def run_test(test):
+    test.initialize()
+    test.run()
+
+def main(args):
+    patterns = args["<patterns>"]
+    tests = list(discover_integration_tests(patterns))
+
+    if len(tests) == 0:
+        logging.warning("No tests match the provided pattern")
+
+    # for test in tests:
+    #     run_test(test)
+    with ThreadPoolExecutor() as pool:
+        pool.map(run_test, tests)
 
 if __name__ == "__main__":
-    main()
+    options = docopt(__doc__)
+    main(options)
 
