3c00410397b153a31fef6c7531b87c1cfc00b083
Michael Bryan <michaelfbryan@gmail.com>
2018-05-30 22:11:01 +0800

    Updated CI stuff

--- ci/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-
-# Only upload the built book to github pages if it's a commit to master
-if [ "$TRAVIS_BRANCH" = master -a "$TRAVIS_PULL_REQUEST" = false  -a -z "$NIGHTLY" ]; then
-  cargo doc
-  travis-cargo doc-upload
-fi
--- ci/install.sh
+++ ci/install.sh
@@ -1,14 +1,6 @@
-#!/bin/bash
-
 set -ex
 
-dependencies() {
-  pip3 install --user ghp-import travis-cargo jinja2 docopt python-dateutil
-}
-
 main() {
-    dependencies
-
     local target=
     if [ $TRAVIS_OS_NAME = linux ]; then
         target=x86_64-unknown-linux-musl
@@ -17,6 +9,26 @@ main() {
         target=x86_64-apple-darwin
         sort=gsort  # for `sort --sort-version`, from brew's coreutils.
     fi
+
+    # Builds for iOS are done on OSX, but require the specific target to be
+    # installed.
+    case $TARGET in
+        aarch64-apple-ios)
+            rustup target install aarch64-apple-ios
+            ;;
+        armv7-apple-ios)
+            rustup target install armv7-apple-ios
+            ;;
+        armv7s-apple-ios)
+            rustup target install armv7s-apple-ios
+            ;;
+        i386-apple-ios)
+            rustup target install i386-apple-ios
+            ;;
+        x86_64-apple-ios)
+            rustup target install x86_64-apple-ios
+            ;;
+    esac
 
     # This fetches latest stable release
     local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
