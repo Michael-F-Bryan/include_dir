c821fade85f515ff24bed132575c9dc7c5b030f0
Michael Bryan <michaelfbryan@gmail.com>
2018-05-30 22:16:13 +0800

    Removed the unnecessary deploy stuff

--- ci/script.sh
+++ ci/script.sh
@@ -2,7 +2,6 @@
 
 set -ex
 
-# TODO This is the "test phase", tweak it as you see fit
 main() {
     cross build --target $TARGET
     cross build --target $TARGET --release
@@ -13,9 +12,6 @@ main() {
 
     cross test --target $TARGET
     cross test --target $TARGET --release
-
-    cross run --target $TARGET
-    cross run --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
