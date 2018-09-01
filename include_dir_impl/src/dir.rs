2deb90173fd977119a9b292823ba643cc20f198d
Michael Lamparski <diagonaldevice@gmail.com>
2018-09-01 11:52:25 -0400

    use include_bytes! for less memory overhead
    This uses `include_bytes!` to generate the bytestring rather than
    the `quote!` macro.  With this change, the total memory used by rustc
    for a crate that includes a 15 MB file drops from over 8 GB to only
    about 700 MB.
    
    This is similar to #23, but that predated the proc_macro.
    
    It should also resolve *some* aspects of #31 (namely for modified
    files), but not all. (the crate still has no way to track new files)

--- include_dir_impl/src/dir.rs
+++ include_dir_impl/src/dir.rs
@@ -6,57 +6,49 @@ use std::path::{Path, PathBuf};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Dir {
-    pub path: PathBuf,
+    pub root_rel_path: PathBuf,
+    pub abs_path: PathBuf,
     pub files: Vec<File>,
     pub dirs: Vec<Dir>,
 }
 
 impl Dir {
-    pub fn from_disk<P: Into<PathBuf>>(path: P) -> Result<Dir, Error> {
-        let path = path.into();
+    pub fn from_disk<Q: AsRef<Path>, P: Into<PathBuf>>(root: Q, path: P) -> Result<Dir, Error> {
+        let abs_path = path.into();
+        let root = root.as_ref();
 
-        if !path.exists() {
+        let root_rel_path = abs_path.strip_prefix(&root).unwrap().to_path_buf();
+
+        if !abs_path.exists() {
             return Err(failure::err_msg("The directory doesn't exist"));
         }
 
         let mut files = Vec::new();
         let mut dirs = Vec::new();
 
-        for entry in path.read_dir().context("Couldn't read the directory")? {
+        for entry in abs_path.read_dir().context("Couldn't read the directory")? {
             let entry = entry?.path();
 
             if entry.is_file() {
-                files.push(File::from_disk(entry)?);
+                files.push(File::from_disk(&root, entry)?);
             } else if entry.is_dir() {
-                dirs.push(Dir::from_disk(entry)?);
+                dirs.push(Dir::from_disk(&root, entry)?);
             }
         }
 
-        Ok(Dir { path, files, dirs })
-    }
-
-    pub fn normalize(&mut self, root: &Path) {
-        self.path = self.path.strip_prefix(root).unwrap().to_path_buf();
-
-        for file in &mut self.files {
-            file.normalize(root);
-        }
-
-        for dir in &mut self.dirs {
-            dir.normalize(root);
-        }
+        Ok(Dir { root_rel_path, abs_path, files, dirs })
     }
 }
 
 impl ToTokens for Dir {
     fn to_tokens(&self, tokens: &mut TokenStream) {
-        let path = self.path.display().to_string();
+        let root_rel_path = self.root_rel_path.display().to_string();
         let files = &self.files;
         let dirs = &self.dirs;
 
         let tok = quote!{
             Dir {
-                path: #path,
+                path: #root_rel_path,
                 files: &[#(
                     #files
                  ),*],
