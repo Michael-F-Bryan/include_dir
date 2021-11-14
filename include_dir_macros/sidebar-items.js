49720817a181922e01bbd05c5cf3f6d3405bb44a
Michael-F-Bryan <michaelfbryan@gmail.com>
2021-11-14 15:28:49 +0000

    Deploying to gh-pages from @ Michael-F-Bryan/include_dir@8c4a4b90da06d65f2abe1762c7eb08852cf99d85 🚀

--- include_dir_macros/sidebar-items.js
+++ include_dir_macros/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"macro":[["include_dir",""]]});
+initSidebarItems({"macro":[["include_dir","Embed the contents of a directory in your crate."]]});
--- source-files.js
+++ source-files.js
@@ -1,6 +1,6 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["glob"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["include_dir"] = {"name":"","files":["dir.rs","dir_entry.rs","file.rs","globs.rs","lib.rs"]};
+sourcesIndex["include_dir"] = {"name":"","files":["dir.rs","dir_entry.rs","file.rs","globs.rs","lib.rs","metadata.rs"]};
 sourcesIndex["include_dir_macros"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["proc_macro2"] = {"name":"","files":["detection.rs","fallback.rs","lib.rs","marker.rs","parse.rs","wrapper.rs"]};
 sourcesIndex["quote"] = {"name":"","files":["ext.rs","format.rs","ident_fragment.rs","lib.rs","runtime.rs","spanned.rs","to_tokens.rs"]};
